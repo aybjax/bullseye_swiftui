66f2efa77f3c73ad0c651a6eb416db5d179ad2d2
aybjax <aybat93@gmail.com>
2021-05-22 23:15:46 +0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by aybjax on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
