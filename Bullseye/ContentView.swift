a8732e1c2a75ad654aab60ec35915b6fae632e53
aybjax <aybat93@gmail.com>
2021-05-23 23:30:02 +0600

    without viewModifier

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,17 +11,59 @@ struct ContentView: View {
         
     // Properties
     // ==========
+    static let max_score = 100
     var sliderInt: Int {
         Int(
             sliderValue.rounded()
         )
     }
     
+    var pointsForCurrentRound: Int {
+        let diff = abs(target - sliderInt)
+        
+        if(diff == 0) {
+            return 200
+        }
+        else if diff == 1 {
+            return 150
+        }
+        
+        return ContentView.max_score - diff
+    }
+    
+    var scoringMessage: String {
+        "The slider value is \(sliderInt).\n" +
+        "The target value is \(target).\n" +
+        "You scored \(pointsForCurrentRound) points this round."
+    }
+    
+    var alertTitle: String {
+        let diff = abs(target - sliderInt)
+        let title: String
+        
+        if diff == 0 {
+            title = "Perfect"
+        }
+        else if diff < 5 {
+            title = "You almost had it"
+        }
+        else if diff <= 10 {
+            title = "Not bad"
+        }
+        else {
+            title = "Are you even trying?"
+        }
+        
+        return title
+    }
+    
     // User interface views
     @State var alertIsVisible = false
-    @State var sliderValue = 50.0
+    @State var sliderValue: Double = Double.random(in: 1...100)
 //    @State var target: Int = .random(in: 1...100)
     @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 0
     
     // User interface content and layout
     var body: some View {
@@ -31,8 +73,14 @@ struct ContentView: View {
             // Target row
             HStack {
                 Text("Put the bullseye as close as you can to:")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
                     .padding()
                 Text("\(target)")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 24))
+                    .foregroundColor(Color.yellow)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
             }
             
             Spacer()
@@ -40,10 +88,16 @@ struct ContentView: View {
             // Slider row
             HStack {
                 Text("1")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
 //                Slider(value: Binding.constant(100))
 //                Slider(value: .constant(100))
                 Slider(value: $sliderValue, in: 1...100)
                 Text("100")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
             }
             
             Spacer()
@@ -51,17 +105,25 @@ struct ContentView: View {
             // Button row
             Button(action: {
                 print("button pressed")
-                self.alertIsVisible = true
+                alertIsVisible = true
             }) {
                 Text("Hit me!")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.black)
             }
+            .background(
+                Image("Button-Normal")
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+            )
             .alert(isPresented: self.$alertIsVisible) {
                 Alert(
-                    title: Text("Hello there"),
+                    title: Text("\(alertTitle)"),
                     message: Text(
-                        scoringMessage()
+                        scoringMessage
                     ),
-                    dismissButton: .default(Text("Awesome!"))
+                    dismissButton: .default(Text("Awesome!")) {
+                        newRound()
+                    }
                 )
             }
             
@@ -69,17 +131,31 @@ struct ContentView: View {
             
             // Score row
             HStack {
-                Button("Start over") {}
+                Button("Start over") {
+                    startGame()
+                }
                 
                 Spacer()
                 
                 Text("Score:")
-                Text("99999")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+                Text("\(score)")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
                 
                 Spacer()
                 
                 Text("Round:")
-                Text("99999")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+                Text("\(round)")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
                 
                 Spacer()
                 
@@ -89,6 +165,14 @@ struct ContentView: View {
             }
             .padding(.bottom, 20)
         }
+        .onAppear() {
+            self.startGame()
+        }
+        .background(
+            Image("Background")
+                .resizable()
+                .scaledToFill()
+        )
     }
     
     
@@ -100,19 +184,19 @@ struct ContentView: View {
         self.alertIsVisible = true
     }
     
-    func pointsForCurrentRound() -> Int {
-        let diff: Int
-        diff = sliderInt > target ?
-                sliderInt - target :
-                target - sliderInt
-        
-        return 100 - diff
+    func startGame() {
+        score = 0
+        round = 1
+        sliderValue = 50
+        target = .random(in: 1...100)
+        sliderValue = Double.random(in: 1...100)
     }
     
-    func scoringMessage() ->String {
-        "The slider value is \(sliderInt).\n" +
-        "The target value is \(target).\n" +
-        "You scored \(pointsForCurrentRound()) points this round."
+    func newRound() {
+        score += pointsForCurrentRound
+        target = .random(in: 1...100)
+        round += 1
+        sliderValue = Double.random(in: 1...100)
     }
 }
 
